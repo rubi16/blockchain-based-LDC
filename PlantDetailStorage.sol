f95b24406555a5aa44a32180bae90fdc746f978c
Rubi Sarkar <54385331+rubi16@users.noreply.github.com>
2023-04-08 21:47:01 +0530

    Update PlantDetailStorage.sol

--- PlantDetailStorage.sol
+++ PlantDetailStorage.sol
@@ -6,6 +6,7 @@ contract plant_Detail{
     struct plant_info{
         uint id;
         uint capacity;
+        uint costperunit;
     }
 
 // var for stroring all data
@@ -14,26 +15,33 @@ contract plant_Detail{
 //array for creating list of var
     plant_info[] public infolist;
 
-    function setStruct(uint _id, uint _capacity) public{
+
+
+
+    function setStruct(uint _id, uint _capacity, uint _costperunit) public{
 
         setinfo.id= _id;
         setinfo.capacity= _capacity;
+        setinfo.costperunit= _costperunit;
     }
 
+      function getStruct() public view returns(uint, uint, uint){
+        return(setinfo.id, setinfo.capacity, setinfo.costperunit);
+    }
+
+
 //fuction for creating list of all plants info
-    function setlist(uint _id, uint _capacity) public{
+    function setlist(uint _id, uint _capacity, uint _costperunit) public{
 
      plant_info memory tempvar = plant_info({
          id: _id,
-         capacity: _capacity
+         capacity: _capacity,
+        costperunit: _costperunit
      });
 
     infolist.push(tempvar);
 
     }
 
-    function getStruct() public view returns(uint, uint){
-        return(setinfo.id, setinfo.capacity);
-    }
-
+  
 }
